9d8c9f7fe0330fbd42f2f516d7f675513aae09ea
serhii_ladniak <sergeyladnyak7@gmail.com>
2024-08-20 01:13:19 +0300

    Ex is done

--- src/main.js
+++ src/main.js
@@ -13,38 +13,56 @@ import { fetchPhotos } from "./js/pixabay-api";
 
 const galleryEl = document.querySelector('.gallery');
 const formElSearch = document.querySelector('.search-form'); 
+const loadEl = document.querySelector('span');
 
 const onSearchSubmit = (event) => {
     event.preventDefault();
+    loadEl.classList.remove("is-hidden");
 
     const searchFromInput = formElSearch.elements.user_query.value;
 
     if (searchFromInput == '') {
         iziToast.error({
             message: 'Sorry, there are no images matching your search query. Please try again!',
+            position: 'topRight',
         })
 
         galleryEl.innerHTML = '';
         formElSearch.reset();
+        loadEl.classList.add("is-hidden");
 
         return;
     }
         fetchPhotos(searchFromInput)
         .then(data => {
-            console.log(data);
 
+            if (data.hits.length === 0) {
+                iziToast.error({
+                    message: 'Sorry, there are no images matching your search query. Please try again!',
+                    position: 'topRight',
+                })
+
+                galleryEl.innerHTML = '';
+                formElSearch.reset();
+                loadEl.classList.add("is-hidden");
+
+                return;
+            }
+            
             const galleryCardTemplate = data.hits.map(imgDetails => createGalleryCard(imgDetails)).join('');
-
             galleryEl.innerHTML = galleryCardTemplate;
 
             let gallery = new SimpleLightbox('.gallery a');
             gallery.refresh();
             gallery.on('show.simplelightbox', function () {});
-
+            
         })
         .catch(error => {
             console.log(error);
         })
+         .finally(() => {
+            loadEl.classList.add("is-hidden");
+        });
     
     formElSearch.reset();
 };
